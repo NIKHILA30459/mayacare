fa7288a4c76481655b2d90ac3ba00c65f3fc330f
Nikhila <nikhilavarma1119@gmail.com>
2022-06-11 18:13:30 +0530

    DonationPage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
 import logo from './logo.svg';
-import './App.css';
-
+import './index.css'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+   <Donate/>
   );
 }
-
+function Donate(){
+  return(
+  <div class="form-container">
+    <form class="register-form">
+      {/* Uncomment the next line to show the success message */}
+      {/* <div class="success-message">Success! Thank you for registering</div> */}
+      <input
+        id="first-name"
+        class="form-field"
+        type="text"
+        placeholder="Full Name"
+        name="firstName"
+      />
+      {/* Uncomment the next line to show the error message */}
+      {/* <span id="first-name-error">Please enter a first name</span> */}
+      <input
+        id="last-name"
+        class="form-field"
+        type="text"
+        placeholder="Email"
+        name="lastName"
+      />
+      {/* Uncomment the next line to show the error message */}
+      {/* <span id="last-name-error">Please enter a last name</span> */}
+      <input
+        id="email"
+        class="form-field"
+        type="text"
+        placeholder="amount"
+        name="email"
+      />
+      {/* Uncomment the next line to show the error message */}
+      {/* <span id="email-error">Please enter an email address</span> */}
+      <button class="form-field" type="submit">
+        Donate
+      </button>
+      
+   
+    <div><h3 class='or'>or</h3></div>
+    <button class="form-field">
+        Log In
+      </button>
+    </form>
+  </div>
+  );
+}
 export default App;
